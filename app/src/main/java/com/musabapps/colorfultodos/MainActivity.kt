68b1cb8fcdddeafb33865de49c4ce83dfc388ad0
Musab Nasredlin <musabagab2010@gmail.com>
2020-08-28 15:34:51 +0300

    setuped the nav component

--- app/src/main/java/com/musabapps/colorfultodos/MainActivity.kt
+++ app/src/main/java/com/musabapps/colorfultodos/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.musabapps.colorfultodos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.todosListFragment
+            )
+        )
+        val controller = findNavController(R.id.nav_host_fragment_container)
+
+        NavigationUI.setupWithNavController(toolbar, controller, appBarConfiguration)
+
+
     }
 }
